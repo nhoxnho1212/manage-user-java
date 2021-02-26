935c2c1287776a8af526444f5423e004b3d5cf55
KMS\tungntdo <tungntdo@kms-technology.com>
2021-02-26 10:03:38 +0700

    Add Configuration for a deployable WAR file.

--- src/main/java/com/tungntdo/demo/config/Constants.java
+++ src/main/java/com/tungntdo/demo/config/Constants.java
@@ -1,10 +1,4 @@
 package com.tungntdo.demo.config;
 
 public class Constants {
-    public interface URL {
-        interface USER {
-            String MAIN = "/users";
-            String LOGIN = MAIN + "/login";
-        }
-    }
 }
