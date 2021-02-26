fff793a7daff0f6a05ae1feee1e929403a8b7d93
KMS\tungntdo <tungntdo@kms-technology.com>
2021-02-26 10:05:35 +0700

    Add connection to PostgresSQL

--- src/main/java/com/tungntdo/demo/config/Constants.java
+++ src/main/java/com/tungntdo/demo/config/Constants.java
@@ -1,4 +1,10 @@
 package com.tungntdo.demo.config;
 
 public class Constants {
+    public interface URL {
+        interface USER {
+            String MAIN = "/users";
+            String LOGIN = MAIN + "/login";
+        }
+    }
 }
